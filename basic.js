c6cc17ad28fa179a6892264bf79fc604889aaaff
Emmanuel B. Giwa <kodagiwa@gmail.com>
2022-06-15 15:26:10 +0100

    first day on JS

---
+++ basic.js
@@ -0,0 +1,25 @@
+function login() {
+  console.log('World'); // Display world in console
+}
+/* let, const */
+// let a = 5;
+// let b = 10;
+// let isMarried = false;
+// let x = 25;
+// let y = 15;
+// let z = 100;
+// BODMAS
+// add +
+// subtract -
+// multiply *
+// divide /
+// modulus %
+// "" or ''
+// let name = "Maria is " + (2022 - 2000) + " " + "years old";
+// let name = "Maria is " + (2022 - 2000) + " years old";
+// let lastName = "Agnes";
+// let fullName = firstName + " " + lastName;
+console.log(name);
+
+
+
